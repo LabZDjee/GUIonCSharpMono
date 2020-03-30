0a4d32db5adf5fbc397c23fe005215b46d33f979
Gérard Gauthier <LabZDjee@users.noreply.github.com>
2020-03-30 09:17:25 +0200

    Additions in ProtectDc
    Introduction of REGSITRY, slaveNumber, and userAbort in ProtectDc
    Source formatting

---
+++ aes-private.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace companySecretAes {
+  public class SecretAES {
+    public static string Key { get { return "B3D29DF7C676A7FD70135F8DE7354DEF5F850FBE74E4CD6D8C516032A929FDE1"; } }
+    public static string InitialisationVector { get { return "86765300A5792AA4602B6B5B475875A5"; } }
+  }
+}
